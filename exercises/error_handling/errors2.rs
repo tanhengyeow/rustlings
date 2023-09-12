c4cfabeb1e7e7be134c0318fbc0e379f896b773c
tanhengyeow <25320782+tanhengyeow@users.noreply.github.com>
2023-09-12 23:38:34 +0800

    add attempts for more exercises

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -17,15 +17,23 @@
 // one is a lot shorter!
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-use std::num::ParseIntError;
+use std::num::{ParseIntError};
+// use std::num::IntErrorKind::InvalidDigit;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
 
+    // First approach
+    // let qty = item_quantity.parse::<i32>();
+    // match qty {
+    //     Ok(qty) => Ok(qty * cost_per_item + processing_fee),
+    //     Err(error) => Err(error)
+    // }
+
+    // Second approach
+    let qty = item_quantity.parse::<i32>()?;
     Ok(qty * cost_per_item + processing_fee)
 }
 
--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,7 +1,6 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
@@ -11,8 +10,8 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        if let word = optional_target {
+            assert_eq!(word, Some(target));
         }
     }
 
@@ -29,8 +28,13 @@ mod tests {
 
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
+        while let integer = optional_integers.pop() {
+            if cursor == 0 {
+                assert_eq!(integer, Some(None));
+                break;
+            } else {
+                assert_eq!(integer, Some(Some(cursor)));
+            }
             cursor -= 1;
         }
 
